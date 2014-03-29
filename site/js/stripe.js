fdbb1d3700589323e71d663301a78aa9a56a5d27
Dan Woodson <woodson.dan@gmail.com>
2014-03-29 00:33:06 +0000

    Multiple changes.

--- site/js/stripe.js
+++ site/js/stripe.js
@@ -1,8 +1,15 @@
-// Checkout
+// Stripe
+// ======
+// Handles all of the client-side payment proocessing
+// related to [Stripe](https://stripe.com/)
+import {ajax} from './util';
 var handler = StripeCheckout.configure({
       key: 'pk_test_AbSR2xBdtLx1oleKC2Lr1Vyy',
       image: '/square-image.png',
       token: function(token, args) {
+        console.log(token);
+        console.log(args);
+        //ajax('/', 'GET', 'data=3000', function(){alert('port');});
         // Use the token to create the charge with a server-side script.
         // You can access the token ID with `token.id`
       }
@@ -11,6 +18,7 @@ var handler = StripeCheckout.configure({
     idx = 0,
     len = nlPurchaseElms.length;
 
+// Attach click handler to all purchase buttons
 for(idx; idx < len; idx++){
   nlPurchaseElms[idx].addEventListener('click', function(e) {
     // Open Checkout with further options

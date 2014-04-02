a14034bc9453ed5b3499d96907a5a4fd9639d15e
Dan Woodson <woodson.dan@gmail.com>
2014-04-02 16:09:16 +0000

    Creating customers instead of just payments

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('newrelic');
 var express = require('express'),
     opts = require('./lib/opts');
     checkout = require('./lib/Checkout');
+    customer = require('./lib/Customer');
     app = express(),
     port = Number(opts.get('PORT'));
 
@@ -10,7 +11,7 @@ app.use(express.static(__dirname + '/client'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.post('/checkout', function(req, res){
-  checkout.process(req.body.token, req.body.customer, function(){console.log('back');});
+  customer.create(req.body.token, req.body.customer, function(){console.log('back');});
   res.send();
 });
 app.listen(port);
